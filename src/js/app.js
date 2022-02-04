ea4bd5280ea8ef54cd0036b2a9c9792d7ecf3451
Sayah Reda <reda1998.rs@gmail.com>
2022-02-04 18:22:18 +0100

    add comment

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,56 @@ import React from 'react';
 import { render } from 'react-dom';
 import Comment from './comment';
 import '../css/style.css';
+import '../css/replie.css';
 import data from '../data/data.json';
+import img_avatar from '../imgs/avatars/image-maxblagun.png';
+import { useEffect, useState } from 'react';
+var v = [data];
 
-import { useState, useEffect, useContext } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 const App = () => {
+  const [rerender, setRerender] = useState(v[0].comments);
+
+  console.log('rerender');
+  var x = 0;
+
+  useEffect(() => {
+    document.getElementById('btn_send').onclick = function() {
+      v[0].comments.push({
+        id: 1,
+        content:
+          "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+        createdAt: '1 month ago',
+        score: 12,
+        user: {
+          image: {
+            png: './images/avatars/image-amyrobson.png',
+            webp: './images/avatars/image-amyrobson.webp'
+          },
+          username: 'amyrobson'
+        },
+        replies: []
+      });
+      setRerender(v[0].comments);
+      x++;
+
+      console.log(rerender);
+    };
+  }, []);
   return (
     <div className="container">
-      <Comment />
-      <Comment />
+      {x}
+      {rerender.map(x => {
+        return <Comment key={x.id} data={x} />;
+      })}
+      <div className="comment_input">
+        <img src={img_avatar} alt="avatar" id="avatar_send" />
+        <textarea
+          type="text"
+          id="comment_entry"
+          placeholder="Add a comment..."
+        />
+        <button id="btn_send">SEND</button>
+      </div>
     </div>
   );
 };
---
+++ src/js/replie.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import img_avatar from '../imgs/avatars/image-amyrobson.png';
+import img_reply from '../imgs/icon-reply.svg';
+import plus from '../imgs/icon-plus.svg';
+import minus from '../imgs/icon-minus.svg';
+
+const Replie = props => {
+  return (
+    <div className="replie_container">
+      <div className="score">
+        <img src={plus} />
+        <p>{props.data.score}</p>
+        <img src={minus} />
+      </div>
+      <div className="comment">
+        <div className="comment_info">
+          <img src={img_avatar} id="avatar" alt="avatar" />
+          <h1 id="user_name">{props.data.user.username}</h1>
+          <p id="time_comment">{props.data.createdAt}</p>
+          <button className="btn">
+            <img src={img_reply} id="reply_img" />
+            Reply
+          </button>
+        </div>
+        <div className="paragraphe">
+          <p id="user_content">{props.data.content}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Replie;
